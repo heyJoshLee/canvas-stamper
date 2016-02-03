82a36654292b33bc78f1f2f177170ac48ac7e0f2
jleewebdev <lee45276@gmail.com>
2016-02-03 14:27:37 +0800

    Added auto select drawing method when page loads

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(function(){
-  
+  var canvas = $("canvas")[0],
+      ctx = canvas.getContext("2d"),
+      method;
+
+  var drawing_methods = {
+    square: function() {
+      var side = 30;
+      ctx.fillRect(0, 0, side, side);
+    }
+  };
+
+  $(".drawing_method").on("click", function(e){
+    e.preventDefault();
+    var $e = $(this)
+        class_name = "active";
+    $e.closest("ul").find("." + class_name).removeClass(class_name);
+    $e.addClass(class_name);
+    method = $e.attr("data-method");
+  }).eq(0).click();
+
+  $("canvas").on("click", function() {
+    drawing_methods[method]();
+  });
 });
